f8f34d3ed7a38d0092fe3d733bce9abfb8e10bf1
Heng <bunsanphe@gmail.com>
2020-08-20 22:04:44 -0700

    adding starting files

---
+++ client/src/App.js
@@ -0,0 +1,19 @@
+import React from "react";
+import Nav from "./components/Nav";
+
+
+// The app will not render correctly until you setup a Route component.
+// Refer to the Basic Example documentation if you need to.
+// (https://reacttraining.com/react-router/web/example/basic)
+function App() {
+  return (
+   
+    <div>
+      <Nav />
+
+    </div>
+   
+  );
+}
+
+export default App;
