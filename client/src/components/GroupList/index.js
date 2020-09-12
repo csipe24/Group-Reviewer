3d856c1f116ba260d190b46f38dc828fbb024089
Nivolving <nivethapartheeban@gmail.com>
2020-09-11 21:22:15 -0700

    stashing current changes

--- client/src/components/GroupList/index.js
+++ client/src/components/GroupList/index.js
@@ -1,78 +1,104 @@
-import React, { useEffect, useState } from 'react'
-import { useStoreContext } from '../../store/index'
-import api from '../../utils/api'
-import { Link } from "react-router-dom"
+import React, { useEffect, useState } from "react";
+import { useStoreContext } from "../../store/index";
+import api from "../../utils/api";
+import { Link } from "react-router-dom";
 // eslint-disable-next-line no-unused-vars
-import { GET_GROUP, REMOVE_GROUP, LOADING } from '../../store/actions'
+import { GET_GROUP, REMOVE_GROUP, LOADING } from "../../store/actions";
 
 // eslint-disable-next-line no-unused-vars
-import { Grommet, CardHeader, Box, Card, CardBody, CardFooter, Button, Heading, Meter } from 'grommet'
+import {
+  Grommet,
+  CardHeader,
+  Box,
+  Card,
+  CardBody,
+  CardFooter,
+  Button,
+  Heading,
+  Meter,
+} from "grommet";
 
-function GroupList () {
-  const [state, dispatch] = useStoreContext()
+function GroupList() {
+  const [state, dispatch] = useStoreContext();
 
   const getGroups = () => {
-    dispatch({ type: LOADING })
-    api.getGroups()
-      .then(results => {
-        console.log(results.data)
-          dispatch({
-            type: GET_GROUP,
-            groups: results.data
-          })
+    dispatch({ type: LOADING });
+    api
+      .getGroups()
+      .then((results) => {
+        console.log(results.data);
+        dispatch({
+          type: GET_GROUP,
+          groups: results.data,
+        });
       })
-      .catch(err => console.log(err))
-  }
+      .catch((err) => console.log(err));
+  };
 
   useEffect(() => {
-    getGroups()
-  }, [])
+    getGroups();
+  }, []);
 
-    const removeGroup = (id) => {
-      console.log(id)
-      api.removeGroup(id).then( () => {
+  const removeGroup = (id) => {
+    console.log(id);
+    api
+      .removeGroup(id)
+      .then(() => {
         dispatch({
           type: REMOVE_GROUP,
-          _id: id
-        })
-      })  .catch(err => console.log(err));
-    }
-
-
+          _id: id,
+        });
+      })
+      .catch((err) => console.log(err));
+  };
 
   return (
     <Grommet plain>
       <Box>
         {state.groups.length ? (
-          <Box >
-            {state.groups.slice(0).reverse().map(group => (
-              <Card key={group._id} width="medium" background="light-1" margin="medium" >
-                <CardHeader pad="small">Group Name:{group.groupName}</CardHeader>
+          <Box>
+            {state.groups
+              .slice(0)
+              .reverse()
+              .map((group) => (
+                <Card
+                  key={group._id}
+                  width="medium"
+                  background="light-1"
+                  margin="medium"
+                >
+                  <CardHeader pad="small">
+                    Group Name:{group.groupName}
+                  </CardHeader>
 
-                <CardFooter pad={{ horizontal: 'medium' }} background="light-2">
-                <Link to={"/group/" + group._id} > 
-                <strong>Go to Posts</strong>
-                </Link>
-                  <Button
-                    primary label="Delete" onClick={() => removeGroup(group._id)}
-                    color="#00739D"
-                  />
-                  {/* <Button label="Update"
+                  <CardFooter
+                    pad={{ horizontal: "medium" }}
+                    background="light-2"
+                  >
+                    <Link to={"/group/" + group._id}>
+                      <strong>Go to Posts</strong>
+                    </Link>
+                    <Button
+                      primary
+                      label="Delete"
+                      onClick={() => removeGroup(group._id)}
+                      color="#00739D"
+                    />
+                    {/* <Button label="Update"
                     onClick={() => { setEditPostID(post._id) }}/>
                   {post._id === editPostID && (
                     <UpdateModal post={post} closeModal={closeModal}/>
                   )} */}
-                </CardFooter>
-              </Card>
-            ))}
+                  </CardFooter>
+                </Card>
+              ))}
           </Box>
         ) : (
           <Heading>No Posts</Heading>
         )}
-
       </Box>
     </Grommet>
-  )
+  );
 }
 
-export default GroupList
+export default GroupList;
--- routes/groupController.js
+++ routes/groupController.js
@@ -1,34 +1,52 @@
 const express = require("express");
 const router = express.Router();
-const passport = require("passport")
+const passport = require("passport");
 
 // const {Group} = require("../models");
-const {Group, Post} = require("../models")
+const { Group, Post } = require("../models");
 
-
-router.post("/newGroup", passport.authenticate('jwt', { session: false }),(req, res) => {
-
+router.post(
+  "/newGroup",
+  passport.authenticate("jwt", { session: false }),
+  (req, res) => {
     const newGroup = new Group({
-        groupName: req.body.groupName,
-        owner: req.user._id,
-        users: [req.user._id]
-      });
+      groupName: req.body.groupName,
+      owner: req.user._id,
+      users: [req.user._id],
+    });
 
-      newGroup.save().then(group => res.json(group))
-      .catch(err => console.log(err));
-})
+    newGroup
+      .save()
+      .then((group) => res.json(group))
+      .catch((err) => console.log(err));
+  }
+);
 
 router.get("/getGroups", (req, res) => {
   Group.find(req.query)
-  .then(req => {res.json(req)})
-  .catch(err => res.status(422).json(err));
-})
+    .then((req) => {
+      res.json(req);
+    })
+    .catch((err) => res.status(422).json(err));
+});
 
 router.get("/group/:id", (req, res) => {
-  Post.find({groupName: req.params.id})
-  .then(groupPosts => {res.json(groupPosts)})
-  .catch(err => { res.status(422).json(err)
-  })
-})
+  Post.find({ groupName: req.params.id })
+    .then((groupPosts) => {
+      res.json(groupPosts);
+    })
+    .catch((err) => {
+      res.status(422).json(err);
+    });
+});
+
+router.delete("/group/:id", (req, res) => {
+  Group.findById({ _id: req.params.id })
+    .then((groupModel) => groupModel.remove())
+    .then((req) => {
+      res.json(req);
+    })
+    .catch((err) => res.status(422).json(err));
+});
 
 module.exports = router;
